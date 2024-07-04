a2b4272ccde8c4f52f8110656638c56e0239e2ae
msm1638 <130028470+msm1638@users.noreply.github.com>
2024-07-04 15:53:04 +0900

    2024-07-04 공부

--- 4_input_div.py
+++ 4_input_div.py
@@ -81,50 +81,124 @@ def reboot_set():
 # SIFT로 특징점 검출, 기술자 계산
 def detect_sift(img):
     sift = cv2.SIFT_create()
-    if img:
-        while True:
-            blurred_frame = cv2.GaussianBlur(img, (5, 5), 0)
-            gray_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2GRAY)
-            keypoint, descriptor = sift.detectAndCompute(gray_frame, None)
+    if img is not None:
+        blurred_frame = cv2.GaussianBlur(img, (5, 5), 0)
+        gray_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2GRAY)
+        detect_data = sift.detectAndCompute(gray_frame, None)
+        return detect_data # keypoints, descriptors
     else:
         print(f"{img} file open error")
-    return descriptor
-# 특징점 매칭 비교
-def match_ratio(descriptor):
-    match_list = []
-    for name in name_info:
-        descriptor_memroy = name_info[name]["memory/"+name]["descriptor"]
-        if descriptor is None or descriptor_memroy is None:
+        return None
+# 특징점 매칭 비교 메인 키만 검색 ex) 축구, 농구
+def match_ratio_main(detect_data):
+    match_list_main = []
+    for main_key in name_info:
+        descriptor_memroy = name_info[main_key]["memory/"+main_key]["descriptor"]
+        if detect_data[1] is None or descriptor_memroy is None:
             continue
-        matches = bf.knnMatch(descriptor, descriptor_memroy, k=2)
+        matches = bf.knnMatch(detect_data[1], descriptor_memroy, k=2)
         good_matches = [m for m, n in matches if m.distance < 0.75 * n.distance]
         if len(matches) > 0:
             match_ratio = len(good_matches) / len(matches)
         else:
             match_ratio = 0.0
-        match_list.append([name, match_ratio])
-    return sorted(match_list, key=lambda x: x[1])
-#####2024-07-03 여기까지 함
-
-
-
-
-
+        match_list_main.append([main_key, match_ratio])
+    return sorted(match_list_main, key=lambda x: x[1], reverse=True)
+# 특징점 매칭 비교 세부 키 검색 ex) 축구-> 패스, 슛, 드리블
+def match_ratio_details(detect_data, match_list_main_select):
+    match_list_details = []
+    main_key = match_list_main_select[0]
+    for detail_key in name_info[main_key]:
+        descriptor_memroy = name_info[main_key][detail_key]["descriptor"]
+        if detect_data[1] is None or descriptor_memroy is None:
+            continue
+        matches = bf.knnMatch(detect_data[1], descriptor_memroy, k=2)
+        good_matches = [m for m, n in matches if m.distance < 0.75 * n.distance]
+        if len(matches) > 0:
+            match_ratio = len(good_matches) / len(matches)
+        else:
+            match_ratio = 0.0
+        match_list_details.append([detail_key, match_ratio])
+    return sorted(match_list_details, key=lambda x: x[1], reverse=True)
 
 # 해당 데이터 이름 지정
-def naming(img, best_match_ratio): # 유사율? 매개변수 받아야함
+def naming(detect_data):
+    best_matche_list = []
+    # 메인 키 중 매치율 제일 높은 이름(나중에 E-greedy로 2위, 3위 등 쓰기)
+    matche_list_main = match_ratio_main(detect_data)
+    for i in range(min(len(matche_list_main), 3)):
+        best_matche_list_main = matche_list_main[i]
+        if best_matche_list_main is not None:  # None 체크 추가
+            matche_list_detail = match_ratio_details(detect_data, best_matche_list_main)
+            for j in range(min(len(matche_list_detail), 3)):
+                best_matche_list_detail = matche_list_detail[j]
+                if best_matche_list_detail is not None:  # None 체크 추가
+                    best_matche_list.append([best_matche_list_main, best_matche_list_detail])
+####2024-07-04 여기까지함@@@
     if auto_mode:
         pass # 구현할 때 naming()에 매개변수 받고, 그걸로 name_info 돌려서 비슷한 거 일정 퍼센트 넘으면 그 이름으로
             # 만약, soccer과 유사한데, 그 중에서 pass와 유사할 경우 pass/pass1, 2, 3 ...
     else:
-        name = input("어떤 이름으로 지정하겠습니까?") # 재정의, 새정의?
+        print(name_info.keys())
+        print(f"현재 가장 유사한 카테고리: {best_matche_list}")
+        main_key = input("어떤 이름으로 지정하겠습니까?") # 재정의, 새정의?
+        if main_key in name_info:
+            print("존재")
+        else:
+            print("미존재")
+        
     return name
 # name_info에 name 저장
 def name_info_save(name, keypoints_list):
     pass
 
+name_info = {
+    "name": {
+        "memory/name": {
+            "keypoints": [],
+            "descriptor": [],
+            "actions": [],
+            "reward": 0
+        },
+        "memory/name/name1": {
+            "keypoints": [],
+            "descriptor": [],
+            "actions": [],
+            "reward": 0
+        },
+        "memory/name/name2": {
+            "keypoints": [],
+            "descriptor": [],
+            "actions": [],
+            "reward": 0
+        }
+    },
+    "soccer": {
+        "memory/soccer": {
+            "keypoints": [],
+            "descriptor": [],
+            "actions": [],
+            "reward": 0
+        },
+        "memory/soccer/pass": {
+            "keypoints": [],
+            "descriptor": [],
+            "actions": [],
+            "reward": 0
+        },
+        "memory/soccer/shoot": {
+            "keypoints": [],
+            "descriptor": [],
+            "actions": [],
+            "reward": 0
+        }
+    }
+}
+naming(0, 0)
 
 
+# 메인 키 검색해서 특정 일치율 넘는 게 없을 경우 & 세부 키에서도 특정 일치율 넘는 게 없을 경우
+# 새로운 메인 키 생성
 
 
 
